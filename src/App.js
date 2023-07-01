50a7b4e461656b374565e3aea945ed3e101c6bc0
kkapi <hZG2fik_j8@mail.ru>
2023-07-01 19:55:15 +0300

    map render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import PostItem from './components/PostItem';
 import './styles/App.css';
 
 function App() {
+	const [posts, setPosts] = useState([
+		{id: 1, title: 'JavaScript1', body: 'Description'},
+		{id: 2, title: 'JavaScript2', body: 'Description'},
+		{id: 3, title: 'JavaScript3', body: 'Description'},
+		{id: 4, title: 'JavaScript4', body: 'Description'},
+		{id: 5, title: 'JavaScript5', body: 'Description'},
+	])
 	return (
 		<div className="App">
-			<PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>
-			<PostItem post={{id: 2, title: 'JavaScript', body: 'Description'}}/>
-			<PostItem post={{id: 3, title: 'JavaScript', body: 'Description'}}/>
-			<PostItem post={{id: 4, title: 'JavaScript', body: 'Description'}}/>			
+			<h1>Список постов</h1>
+			{posts.map(post => 
+				<PostItem post={post} key={post.id}/>
+			)}
 		</div>
 	);
 }
