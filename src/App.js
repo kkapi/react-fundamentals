ed8cc0fcadbdd39b92953ecd652a8d5498396ea4
kkapi <hZG2fik_j8@mail.ru>
2023-07-01 20:46:21 +0300

    UI components & module css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { useState } from 'react';
-import PostItem from './components/PostItem';
 import './styles/App.css';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
 	const [posts, setPosts] = useState([
-		{id: 1, title: 'JavaScript1', body: 'Description'},
-		{id: 2, title: 'JavaScript2', body: 'Description'},
-		{id: 3, title: 'JavaScript3', body: 'Description'},
-		{id: 4, title: 'JavaScript4', body: 'Description'},
-		{id: 5, title: 'JavaScript5', body: 'Description'},
-	])
+		{ id: 1, title: 'JavaScript1', body: 'Description' },
+		{ id: 2, title: 'JavaScript2', body: 'Description' },
+		{ id: 3, title: 'JavaScript3', body: 'Description' },
+		{ id: 4, title: 'JavaScript4', body: 'Description' },
+		{ id: 5, title: 'JavaScript5', body: 'Description' },
+	]);	
+
 	return (
 		<div className="App">
-			<h1>Список постов</h1>
-			{posts.map(post => 
-				<PostItem post={post} key={post.id}/>
-			)}
+			<form>
+				<input type='text' placeholder='Название поста'/>
+				<input type='text' placeholder='Описание поста'/>
+				<MyButton>Создать</MyButton>
+			</form>
+			<PostList posts={posts} title={'Спиок постов 1'} />
 		</div>
 	);
 }
