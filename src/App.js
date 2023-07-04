c1d31b956b1418eb4a27c322334f7c456b825840
kkapi <hZG2fik_j8@mail.ru>
2023-07-04 22:32:17 +0300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,81 +1,19 @@
-import { useEffect, useState } from 'react';
-import PostService from './API/PostService';
-import PostFilter from './components/PostFilter';
-import PostForm from './components/PostForm';
-import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyLoader from './components/UI/loader/MyLoader';
-import MyModal from './components/UI/modal/MyModal';
-import MyPagination from './components/UI/pagination/MyPagination';
-import { useFetching } from './hooks/useFetching';
-import { usePosts } from './hooks/usePost';
-import './styles/App.css';
-import { getPageCount, getPagesArray } from './utils/pages';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './components/Navbar/NavBar';
+import About from './pages/About';
+import Posts from './pages/Posts';
 
-function App() {
-	const [posts, setPosts] = useState([]);
-	const [filter, setFilter] = useState({ sort: '', query: '' });
-	const [modal, setModal] = useState(false);
-	const [totalPages, setTotalPages] = useState(0);
-	const [limit, setLimit] = useState(10);
-	const [page, setPage] = useState(1);
-	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-	const [pages, setPages] = useState([]);
-
-	const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-		const response = await PostService.getLimit(limit, page);
-		setPosts(response.data);
-		const totalCount = response.headers['x-total-count'];
-		setPages(getPagesArray(getPageCount(totalCount, limit)));
-		setTotalPages(getPageCount(totalCount, limit));
-	});
-
-	const createPost = newPost => {
-		setPosts([...posts, newPost]);
-		setModal(false);
-	};
-
-	const removePost = post => {
-		setPosts(posts.filter(p => post.id !== p.id));
-	};
-
-	useEffect(() => {
-		fetchPosts();
-	}, [page]);
-
+const App = () => {
 	return (
-		<div className="App">
-			<MyModal visible={modal} setVisible={setModal}>
-				<PostForm create={createPost} />
-			</MyModal>
-			<PostFilter
-				style={{ margin: '10px 0' }}
-				filter={filter}
-				setFilter={setFilter}
-			/>
-			<MyButton onClick={() => setModal(true)}>Создать пост</MyButton>
-			{postError && <h1>Произошла ошибка {postError}</h1>}
-			{isPostsLoading ? (
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'center',
-						marginTop: '50px',
-					}}
-				>
-					<MyLoader />
-				</div>
-			) : (
-				<PostList
-					remove={removePost}
-					posts={sortedAndSearchedPosts}
-					title={'Спиcок постов 1'}
-				/>
-			)}
-			<MyPagination pages={pages} setPage={setPage} page={page} />
-		</div>
+		<BrowserRouter>
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<About />} />
+				<Route path="/posts" element={<Posts />} />
+			</Routes>
+		</BrowserRouter>
 	);
-}
+};
 
 export default App;
---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cl from './Navbar.module.css';
+
+const NavBar = () => {
+	return (
+		<div className={cl.navbar}>
+			<div>test</div>
+		</div>
+	);
+};
+
+export default NavBar;
